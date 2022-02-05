86da1bd3b202e447518b76b50da1cd6700ea7892
Alisa Ts <freiheiit92@mail.ru>
2022-02-05 10:37:18 +0300

    Initial Commit

---
+++ myContactList/myContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  myContactListApp.swift
+//  myContactList
+//
+//  Created by Alisa Ts on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct myContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
